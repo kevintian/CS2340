b52c9a20a29ad8810af7d19769dd7f088546876a
kevintian <kevintian@gatech.edu>
2017-03-25 17:39:11 -0400

    Added framework for graphs

--- app/src/main/java/com/nutsandbolts/splash/Controller/GenerateGraphActivity.java
+++ app/src/main/java/com/nutsandbolts/splash/Controller/GenerateGraphActivity.java
@@ -3,6 +3,9 @@ package com.nutsandbolts.splash.Controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.nutsandbolts.splash.R;
 
 public class GenerateGraphActivity extends AppCompatActivity {
@@ -11,5 +14,17 @@ public class GenerateGraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_graph);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        graph.getGridLabelRenderer().setVerticalAxisTitle("Vertical");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("Horizontal");
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
