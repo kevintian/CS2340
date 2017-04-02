7fedf03f7ffe8f87d13ad8dbd626e83fa9741623
Suprabhat Gurrala <supragurrala@gmail.com>
2017-04-02 18:12:02 -0400

    Fixed Code inspection issues

--- app/src/main/java/com/nutsandbolts/splash/Model/UserType.java
+++ app/src/main/java/com/nutsandbolts/splash/Model/UserType.java
@@ -1,7 +1,13 @@
 package com.nutsandbolts.splash.Model;
 
-
+/**
+ * Enum to describe the 4 types of users.
+ */
 public enum UserType {
+    /**
+     * Code inspection will show these as unused declarations, but they are necessary for the
+     * spinners to work properly.
+     */
     CONTRIBUTOR ("Contributor"),
     WORKER ("Worker"),
     MANAGER ("Manager"),
@@ -16,15 +22,6 @@ public enum UserType {
      */
     UserType(String name) {
         this.name = name;
-    }
-
-    /**
-     * Returns a string representation of the user's usertype
-     *
-     * @return  The user's usertype
-     */
-    public String getName() {
-        return name;
     }
 
     @Override
